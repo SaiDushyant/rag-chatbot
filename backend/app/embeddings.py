de3922dcf02b858dfdbabca568ea9f890353f809
SaiDushyant <saidushyant04@gmail.com>
2026-04-15 14:13:48 +0530

    Migrated embeddings to google.genai client

--- backend/app/embeddings.py
+++ backend/app/embeddings.py
@@ -1,12 +1,21 @@
-from sentence_transformers import SentenceTransformer
+from google import genai
+import os
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+# Initialize client
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 
 def generate_embeddings(chunks):
-    embeddings = model.encode(chunks)
-    return embeddings
+    response = client.models.embed_content(
+        model="models/gemini-embedding-001", contents=chunks
+    )
+
+    return [e.values for e in response.embeddings]
 
 
 def embed_query(query: str):
-    return model.encode([query])[0]
+    response = client.models.embed_content(
+        model="models/gemini-embedding-001", contents=query
+    )
+
+    return response.embeddings[0].values
